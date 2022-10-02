472ede45486f82ac927011440ebd80123c6c213a
Sabina Omarova <sabomar170@gmail.com>
2022-10-02 14:57:11 -0400

    Ticketing Part-3

---
+++ src/main/java/com/cydeo/enums/Status.java
@@ -0,0 +1,18 @@
+package com.cydeo.enums;
+
+public enum Status {
+    
+    OPEN ("Open"),
+    IN_PROGRESS ("In Progress"),
+    COMPLETED ("Completed");
+
+    private final String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    Status(String value) {
+        this.value = value;
+    }
+}
---
+++ src/main/java/com/cydeo/service/ProjectService.java
@@ -0,0 +1,7 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.RoleDTO;
+
+public interface ProjectService extends CrudService<ProjectDTO,String> {
+}
