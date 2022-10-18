9b4073bf3aac7044aae801e97624f61f8e6cf109
Sabina Omarova <sabomar170@gmail.com>
2022-10-18 14:20:57 -0400

    Ticketing Part-5

--- src/main/java/com/cydeo/dto/ProjectDTO.java
+++ src/main/java/com/cydeo/dto/ProjectDTO.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Data
@@ -13,14 +15,26 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class ProjectDTO {
 
+    @NotBlank
     private String projectName;
+
+    @NotBlank
     private String projectCode;
+
+    @NotNull
     private UserDTO assignedManager;
-    @DateTimeFormat(pattern="yyyy-MM-dd")
+
+    @NotNull
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate startDate;
-    @DateTimeFormat(pattern="yyyy-MM-dd")
+
+    @NotNull
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate endDate;
+
+    @NotBlank
     private String projectDetail;
+
     private Status projectStatus;
 
     // we need them on project-status page for Project List table
