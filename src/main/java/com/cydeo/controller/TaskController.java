dd7817f69c0ae5e6dee8582253e33535e6193e23
Sabina Omarova <sabomar170@gmail.com>
2022-10-18 14:16:05 -0400

    Ticketing Part-5

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -7,10 +7,10 @@ import com.cydeo.service.TaskService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/task")
@@ -39,9 +39,18 @@ public class TaskController {
     }
 
 
-    @PostMapping("/create")  // save function
-    public String insertTask(TaskDTO task){
+    @PostMapping("/create") // save function
+    public String insertTask(@Valid @ModelAttribute("task") TaskDTO task, BindingResult bindingResult, Model model) {
 
+        if (bindingResult.hasErrors()) {
+
+            model.addAttribute("projects", projectService.findAll());
+            model.addAttribute("employees", userService.findAllEmployees());
+            model.addAttribute("tasks", taskService.findAll());
+
+            return "/task/create";
+
+        }
         taskService.save(task);
 
         return "redirect:/task/create";
@@ -76,7 +85,6 @@ public class TaskController {
 //        return "redirect:/task/create";
 //    }
 
-    @PostMapping("/update/{id}") // update function - 2nd step - save/update this changes in DB(map)
 
     //{id} - we should put, because we don't have this id in the task form
     // whenever we click save after making changes, we need to catcher id of this taskDTO that we did change,
@@ -85,7 +93,18 @@ public class TaskController {
     // in PostMapping : @PathVariable and .setId() - don't need to put in Spring-Boot, IF! : there is same filed name:
     // the field name in {} backers ({id}) exactly the same filed name in object TaskDto (id)
     // it is automatically parsing {id}-value to id field
-    public String updateTask(TaskDTO task){
+    @PostMapping("/update/{id}") // update function - 2nd step - save/update this changes in DB(map)
+    public String updateTask(@Valid @ModelAttribute("task") TaskDTO task, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+
+            model.addAttribute("projects", projectService.findAll());
+            model.addAttribute("employees", userService.findAllEmployees());
+            model.addAttribute("tasks", taskService.findAll());
+
+            return "/task/update";
+
+        }
 
         taskService.update(task);
 
@@ -127,7 +146,16 @@ public class TaskController {
     }
     //2nd step for update function on pending task page:
     @PostMapping("/employee/update/{id}")
-    public String employeeUpdateTask(TaskDTO task){
+    public String employeeUpdateTask(@Valid @ModelAttribute("task") TaskDTO task, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+
+            model.addAttribute("statuses", Status.values());
+            model.addAttribute("tasks", taskService.findAllTasksByStatusIsNot(Status.COMPLETED));
+
+            return "/task/status-update";
+
+        }
 
         taskService.updateStatus(task);
 
