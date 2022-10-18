65cd172c09829bd91eb4ca2237a5fdaeec575640
Sabina Omarova <sabomar170@gmail.com>
2022-10-18 14:22:46 -0400

    Ticketing Part-5

--- src/main/java/com/cydeo/dto/UserDTO.java
+++ src/main/java/com/cydeo/dto/UserDTO.java
@@ -14,23 +14,116 @@ import javax.validation.constraints.*;
 public class UserDTO {
 
     @NotBlank
-    @Size(max=15, min=2)
+    @Size(max = 15, min = 2)
     private String firstName;
+
     @NotBlank
-    @Size(max=15, min=2)
+    @Size(max = 15, min = 2)
     private String lastName;
+
     @NotBlank
     @Email
-    private String userName; //email
-    @Pattern(regexp = "(?=.\\d)(?=.[a-z])(?=.*[A-Z]).{4,}")
-    private String password;
+    private String userName;
+
+    @NotBlank
+    @Pattern(regexp = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,}")
+    private String passWord;
+
     @NotNull
-    private String confirmPassword;
+    private String confirmPassWord;
+
     private boolean enabled;
+
+    @NotBlank
     @Pattern(regexp = "^\\d{10}$")
     private String phone;
+
     @NotNull
     private RoleDTO role;
+
     @NotNull
     private Gender gender;
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+        checkConfirmPassword();
+    }
+
+    public String getConfirmPassWord() {
+        return confirmPassWord;
+    }
+
+    public void setConfirmPassWord(String confirmPassWord) {
+        this.confirmPassWord = confirmPassWord;
+        checkConfirmPassword();
+    }
+
+    private void checkConfirmPassword() {
+        if (this.passWord == null || this.confirmPassWord == null) {
+            return;
+        } else if (!this.passWord.equals(this.confirmPassWord)) {
+            this.confirmPassWord = null;
+        }
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public RoleDTO getRole() {
+        return role;
+    }
+
+    public void setRole(RoleDTO role) {
+        this.role = role;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
 }
